ad465ca86791a4090898dcc76a2b4735432fc38a
Alex <alej.sglez@gmail.com>
2019-03-06 17:11:45 -0500

    added get styles, backgroundcolor and color

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+
 let darkmodefunc = function() {
 
 let getBody = document.querySelector("body");
 
 let getName = document.querySelector("#name");
+
+getBody.style.backgroundcolor = "white";
+
+getName.style.color = "black";
 
 }
